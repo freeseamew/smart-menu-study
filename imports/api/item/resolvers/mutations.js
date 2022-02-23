697ea874cb8c859ab97635a5cdf951a53ebafbd8
freeseamew <freeseamew@gmail.com>
2022-02-23 18:01:48 +0900

    item mutation, apollo-server 세팅 추가

---
+++ imports/api/item/resolvers/mutations.js
@@ -0,0 +1,101 @@
+import { Categories, Items } from '../collections';
+import { getCurrentDate } from '/imports/utils/formatDate';
+
+const mutations = {
+  async addCategory(_, { categoryname }) {
+    const categoryValue = {
+      categoryName: categoryname,
+    }
+
+    try {
+      const result = await Categories.insert(categoryValue);
+      return result;
+    }
+    catch(error) {
+      throw `addCategory Error: ${error}`;
+    }
+  },
+  async updateCategory(_, { _id, categoryName }) {
+    const categoryValue = {
+      categoryName: categoryName,
+    }
+
+    try {
+      const result = await Categories.update(
+        {_id: _id},
+        {$set: categoryValue}
+      )
+      return result;
+    }
+    catch(error) {
+      throw `updateCategory Error: ${error}`;
+    }
+  },
+  async deleteCategory(_, {id}) {
+    try {
+      const result = await Categories.remove(_id);
+      return result;
+    }
+    catch(error) {
+      throw `deleteCategory Error: ${error}`
+    }
+  },
+  async addItem(_, { itemName, itemPrice, itemImage, itemCategoryId }) {
+    const newDate = getCurrentDate();
+
+    const itemValues = {
+      itemName: itemName,
+      itemPrice: itemPrice,
+      itemImage: itemImage,
+      createdAt: newDate,
+      itemCategoryId: itemCategoryId,
+    }
+
+    try {
+      const result = await Items.insert(itemValues);
+      itemValues._id = result;
+
+      return itemValues;
+    }
+    catch(error) {
+      throw `addItem Error: ${error}`;
+    }
+  },
+  async updateItem(_, { _id, itemName, itemPrice, itemImage, itemCategoryId }) {
+
+    const itemValues = {
+      itemName: itemName,
+      itemPrice: itemPrice,
+      itemImage: itemImage,
+      itemCategoryId: itemCategoryId,
+    }
+
+    try {
+      await Items.update(
+        {_id, _id},
+        {$set: itemValues},
+      )
+
+      itemValues._id = _id;
+
+      return itemValues;
+    }
+    catch(error) {
+      throw `updateItem Error: ${error}`;
+    }
+  },
+  async deleteItem(_, { _id }) {
+    try {
+      await Items.remove(_id);
+      return _id;
+    }
+    catch(error) {
+      `deleteItem Error: ${error}`;
+    }
+  },
+  async updateFile(_, {}) {
+    return;
+  },
+}
+
+export default mutations;
---
+++ imports/startup/apollo-server.js
@@ -0,0 +1,37 @@
+import { ApolloServer } from 'apollo-server-express';
+import { makeExecutableSchema } from '@graphql-tools/schema';
+import { WebApp } from 'meteor/webapp';
+import { ApolloServerPluginLandingPageGraphQLPlayground } from 'apollo-server-core';
+
+import resolverItem from '/imports/api/item/resolvers';
+import typeDefsItem from '/imports/api/item/schemas';
+import resolverOrder from '/imports/api/order/resolvers';
+import typeDefsOrder from '/imports/api/order/schemas';
+
+(async function(){
+  const typeDefs = [typeDefsItem, typeDefsOrder];
+  const resolvers = [resolverItem, resolverOrder];
+
+  const schema = makeExecutableSchema({
+    typeDefs,
+    resolvers,
+  });
+
+  const server = new ApolloServer({
+    playground: true,
+    schema,
+    context: '',
+    plugin: [
+      ApolloServerPluginLandingPageGraphQLPlayground(),
+    ]
+  });
+
+  await server.start();
+
+  server.applyMiddleware({
+    app: WebApp.connectHandlers,
+    cors: true,
+    path: '/graphql',
+  });
+
+})();
